e4e99a4f1221c12645b12291e0b3eaf4ac32d313
DavidTaylor96 <david_taylor96@outlook.com>
2022-02-20 12:29:40 +0000

    fix: removed files that aren't needed

--- screens/home-page/home-page.tsx
+++ screens/home-page/home-page.tsx
@@ -1,33 +1,24 @@
-import { Entypo } from "@expo/vector-icons";
-import { StatusBar } from "expo-status-bar";
-import * as React from "react";
-import { StyleSheet, Image } from "react-native";
-import { CardInner } from "../../components/card-inner/card-inner";
-import { Cards } from "../../components/card/card";
-import { View, Text } from "../../components/Themed";
-import useDefaultSafeView from "../../hooks/useDefaultSafeView";
+
+import * as React from 'react';
+import { StyleSheet, Image } from 'react-native';
+import { View, Text } from '../../components/Themed';
+import useDefaultSafeView from '../../hooks/useDefaultSafeView';
 
 export default function HomeScreen() {
-
   const insets = useDefaultSafeView();
 
   return (
-    <>
-      <View style={[styles.wrapper, {paddingTop: insets.top}]}>
-        <Cards color={true}>
-          <CardInner name={"David Taylor"} amount={1000} />
-        </Cards>
-      </View>
-      <StatusBar style={"light"} />
-    </>
+    <View style={[styles.wrapper, { paddingTop: insets.top }]}>
+      <Text>Hello world</Text>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   wrapper: {
-    display: "flex",
+    display: 'flex',
     flex: 1,
-    alignItems: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
   },
- 
 });
