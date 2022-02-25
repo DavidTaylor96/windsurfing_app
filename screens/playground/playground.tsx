c83afeb07c46485e839a07779e3545f81e2c693c
DavidTaylor96 <david_taylor96@outlook.com>
2022-02-25 21:44:30 +0000

    feat: call list item compoent

--- screens/playground/playground.tsx
+++ screens/playground/playground.tsx
@@ -1,22 +1,17 @@
 import * as React from 'react';
-import { StyleSheet, Image } from 'react-native';
-import { View, Text } from '../../components/Themed';
-import useDefaultSafeView from '../../hooks/useDefaultSafeView';
+import { StyleSheet } from 'react-native';
+import { ListItem } from '../../components/list-item/list-item';
+import { Text, View } from '../../components/Themed';
 
 export const Playground = () => {
   return (
     <View style={styles.wrapper}>
-      <Text weight="h1">Hello world</Text>
-      <View style={styles.spacer} />
-      <Text weight="h2">Hello world</Text>
-      <View style={styles.spacer} />
-      <Text weight="h3">Hello world</Text>
-      <View style={styles.spacer} />
-      <Text weight="h4">Hello world</Text>
-      <View style={styles.spacer} />
-      <Text weight="h5">Hello world</Text>
-      <View style={styles.spacer} />
-      <Text weight="normal">Hello world</Text>
+
+
+      <ListItem onPress={() => alert('batman')}>
+        <Text weight="h3">FindHorn</Text>
+      </ListItem>
+
     </View>
   );
 };
@@ -25,7 +20,6 @@ const styles = StyleSheet.create({
   wrapper: {
     display: 'flex',
     flex: 1,
-    alignItems: 'center',
     justifyContent: 'center',
   },
 
