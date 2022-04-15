46e6b4936636b3becc58eeeaa844504c289cbd0a
DavidTaylor96 <david_taylor96@outlook.com>
2022-04-15 22:36:49 +0100

    feat: basic jest set up

---
+++ jest.setup.ts
@@ -0,0 +1,33 @@
+// import mockSafeAreaContext from 'react-native-safe-area-context/jest/mock';
+
+// jest.mock('react-native-safe-area-context', () => mockSafeAreaContext );
+
+jest.mock(
+  '@expo/vector-icons/build/vendor/react-native-vector-icons/lib/create-icon-set.js',
+  () => {
+    return () => '';
+  }
+);
+
+jest.mock('moti', () => {
+  return () => '';
+});
+jest.mock('moti/interactions', () => {
+  return () => '';
+});
+jest.mock('@react-native-mapbox-gl/maps', () => {
+  return () => '';
+});
+
+
+const mockedNavigate = jest.fn();
+
+jest.mock('@react-navigation/core', () => {
+  const actualNav = jest.requireActual('@react-navigation/core');
+  return {
+    ...actualNav,
+    useNavigation: () => ({
+      navigate: mockedNavigate,
+    }),
+  };
+});
